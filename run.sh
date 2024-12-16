dbd4192ed35036f447b8e51c306d69207d789069
Marc Xu <marc.xu@yabbadatadoo.io>
2024-12-16 23:12:48 +0100

    added added experiment config to run all adaptions

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:/UPISAS
-python3 experiment-runner/experiment-runner/ UPISAS/experiment_runner_configs/dingnet.py
+#rm -r UPISAS/experiment_runner_configs/experiments
+# run all experiments
+python3 experiment-runner/experiment-runner/ UPISAS/experiment_runner_configs/dingnet_all.py
